398144274ad08c034d6de7e847185c090aa4c60f
YUHUI LIU <lukas@r086045.fna.swh.mhn.de>
2024-09-24 19:23:12 +0800

    add copy right

---
+++ docs/assets/KeynoteSpeakersView-3490cd89.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as a,a as s}from"./index-45af8f20.js";const n={},_={class:"keynote-speakers"};function c(r,e){return o(),a("div",_,e[0]||(e[0]=[s("div",{class:"title1 font-merri"},"Keynote Speakers",-1),s("p",{class:"pa-content p"},"TBD",-1)]))}const d=t(n,[["render",c],["__scopeId","data-v-9e570801"]]);export{d as default};
