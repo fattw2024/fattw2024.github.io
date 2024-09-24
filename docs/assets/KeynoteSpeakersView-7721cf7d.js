f387c5927aaa48ecd53da5a1b9a3a9e585b8415e
YUHUI LIU <lukas@r086045.fna.swh.mhn.de>
2024-09-24 16:26:07 +0800

    updated content

--- docs/assets/KeynoteSpeakersView-7721cf7d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o,c as a,a as s}from"./index-1b0ec12c.js";const n={},_={class:"keynote-speakers"};function c(r,e){return o(),a("div",_,e[0]||(e[0]=[s("div",{class:"title1 font-merri"},"Keynote Speakers",-1),s("p",{class:"pa-content p"},"TBD",-1)]))}const d=t(n,[["render",c],["__scopeId","data-v-9e570801"]]);export{d as default};
--- docs/assets/ProgramView-76caad16.js
+++
@@ -1,1 +0,0 @@
-import{d as a,o,c as s,a as t,_}from"./index-1b0ec12c.js";const r={class:"program"},c=a({__name:"ProgramView",setup(n){return(d,e)=>(o(),s("div",r,e[0]||(e[0]=[t("p",{class:"pa-content p"},"TBD",-1)])))}});const m=_(c,[["__scopeId","data-v-db1c8dad"]]);export{m as default};
