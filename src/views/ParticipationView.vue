092a7ad02424abe89cee4026703516a3f9ba6fae
YUHUI LIU <lukas@r086045.fna.swh.mhn.de>
2024-09-24 12:16:50 +0800

    update content

--- src/views/ParticipationView.vue
+++ src/views/ParticipationView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="participation">
-    <p class="pa-header p">The WDMD 2024 workshop will take place on October 28th, in Tsukuba, Japan, co-located with ISSRE 2024 (<a href="https://issre.github.io/2024/" target="_blank">https://issre.github.io/2024/</a>).</p>
+    <p class="pa-header p">The FATTW 2024 workshop will take place on October 28th, in Tsukuba, Japan, co-located with ISSRE 2024 (<a href="https://issre.github.io/2024/" target="_blank">https://issre.github.io/2024/</a>).</p>
     <div class="title2">Registration</div>
     <p class="pa-header p">The registration process is managed by the ISSRE committee. Options are available for registering for the full week of the ISSRE conference and for one-day registrations for just the day of the workshop. More information is available at: </p>
     <p class="pa-header p"><a href="https://issre.github.io/2024/attend_registration.html" target="_blank">https://issre.github.io/2024/attend_registration.html</a></p>
